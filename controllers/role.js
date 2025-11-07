b0081313daeec9873fac7080467cdc6c1025bac1
Henshaw <josephhensh2000@gmail.com>
2025-11-07 14:28:20 +0100

    Made updates

--- controllers/role.js
+++ controllers/role.js
@@ -1,13 +1,10 @@
 const asyncHandler = require("../middlewares/asyncHandler");
 const { 
     addRole,
-<<<<<<< HEAD
-    updateRole,
-    deleteRole
-=======
     getRole,
     getRoles,
->>>>>>> c6fbf3f2ca4e82ba055d4120178a8f4dc21eec94
+    updateRole,
+    deleteRole
 } = require("../services/role");
 
 exports.addRole = asyncHandler(async (req, res, next) => {
@@ -19,7 +16,23 @@ exports.addRole = asyncHandler(async (req, res, next) => {
     })
 });
 
-<<<<<<< HEAD
+exports.getRole = asyncHandler(async (req, res, next) => {
+    const result = await getRole(req, res, next);
+    res.status(200).json({
+        "success": true,
+        "message": "Roles",
+        "data": result
+    })
+});
+
+exports.getRoles = asyncHandler(async (req, res, next) => {
+    const result = await getRoles(req, res, next);
+    res.status(200).json({
+        "success": true,
+        "message": "Roles",
+        "data": result
+    })
+});
 
 exports.updateRole = asyncHandler(async (req, res, next) => {
     const result = await updateRole(req, res, next);
@@ -38,22 +51,4 @@ exports.deleteRole = asyncHandler(async (req, res, next) => {
         "message": "Role deleted successful",
         "data": result
     });
-=======
-exports.getRole = asyncHandler(async (req, res, next) => {
-    const result = await getRole(req, res, next);
-    res.status(200).json({
-        "success": true,
-        "message": "Roles",
-        "data": result
-    })
-});
-
-exports.getRoles = asyncHandler(async (req, res, next) => {
-    const result = await getRoles(req, res, next);
-    res.status(200).json({
-        "success": true,
-        "message": "Roles",
-        "data": result
-    })
->>>>>>> c6fbf3f2ca4e82ba055d4120178a8f4dc21eec94
 });
